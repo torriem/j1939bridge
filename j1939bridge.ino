0793e833665789ad41cc5c137058e7292de3503e
Michael Torrie <torriem@gmail.com>
2020-02-11 19:14:12 -0700

    forgot to process Can1 events

--- j1939bridge.ino
+++ j1939bridge.ino
@@ -1,17 +1,66 @@
 /*
-	A simple CAN bridge with j1939 decoding.
+	A simple CAN bridge with j1939 decoding for the Arduino
+	Due or the Teensy 4.0 (requires two CAN ports, so won't
+	work on the Teensy 3.6)
 
 	Requires the following libraries installed into Arduino:
 
 	https://github.com/collin80/can_common
 	https://github.com/collin80/due_can
- */
 
-#include "variant.h" 
-#include <due_can.h>
+	Requires the following library for Teensy 4.0:
 
+	https://github.com/tonton81/FlexCAN_T4
+*/
+
+#ifdef ARDUINO_TEENSY40
+#define TEENSY 1
+#endif
+
+#ifdef TEENSY
+#  include <FlexCAN_T4.h>
+#else //Due
+#  include "variant.h" 
+#  include <due_can.h>
 //if needing higher speed output use the native USB port
 //#define Serial SerialUSB
+#endif
+
+#ifdef TEENSY
+//Union for parsing CAN bus data messages. Warning:
+//Invokes type punning, but this works here because
+//CAN bus data is always little-endian (or should be)
+//and the ARM processor on these boards is also little
+//endian.
+typedef union {
+    uint64_t uint64;
+    uint32_t uint32[2]; 
+    uint16_t uint16[4];
+    uint8_t  uint8[8];
+    int64_t int64;
+    int32_t int32[2]; 
+    int16_t int16[4];
+    int8_t  int8[8];
+
+    //deprecated names used by older code
+    uint64_t value;
+    struct {
+        uint32_t low;
+        uint32_t high;
+    };
+    struct {
+        uint16_t s0;
+        uint16_t s1;
+        uint16_t s2;
+        uint16_t s3;
+    };
+    uint8_t bytes[8];
+    uint8_t byte[8]; //alternate name so you can omit the s if you feel it makes more sense
+} BytesUnion;
+
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can1;
+#endif
 
 static inline void print_hex(uint8_t *data, int len) {
 	char temp[4];
@@ -57,16 +106,22 @@ void j1939Decode(long ID, unsigned long* PGN, byte* priority, byte* src_addr, by
 	}
 }
 
-void got_frame(CAN_FRAME *frame, int which) {
+void got_frame(uint32_t id, uint8_t extended, uint8_t length, 
+               BytesUnion *data, uint8_t which_interface) {
 	unsigned long PGN;
 	byte priority;
 	byte srcaddr;
 	byte destaddr;
 
-	j1939Decode(frame->id, &PGN, &priority, &srcaddr, &destaddr);
+	j1939Decode(id, &PGN, &priority, &srcaddr, &destaddr);
 
 	//could filter out what we want to look at here on any of these
 	//variables.
+	if(which_interface)
+		Serial.print("1->0,");
+	else
+		Serial.print("0->1,");
+
 	Serial.print(PGN);
 	Serial.print(",");
 	Serial.print(priority);
@@ -75,37 +130,87 @@ void got_frame(CAN_FRAME *frame, int which) {
 	Serial.print(",");
 	Serial.print(destaddr);
 	Serial.print(",");
-	print_hex(frame->data.bytes, frame->length);
+	print_hex(data->bytes, length);
 
 	//example of a decode
 	if (PGN == 65267) { //vehicle position message
 		//latitude
-		Serial.print(frame->data.uint32[0] / 10000000.0 - 210.0);
+		Serial.print(data->uint32[0] / 10000000.0 - 210.0);
 		Serial.print(",");
 		//longitude
-		Serial.println(frame->data.uint32[1] / 10000000.0 - 210.0);
-	}
-
-	if (which == 0) {
-		//transmit it out Can1
-		Can1.sendFrame(*frame);
-	} else {
-		//transmit it out Can0
-		Can0.sendFrame(*frame);
+		Serial.println(data->uint32[1] / 10000000.0 - 210.0);
 	}
 }
 
-void can0_got_frame(CAN_FRAME *frame) {
-	got_frame(frame,0);
+#ifdef TEENSY
+void can0_got_frame_teensy(const CAN_message_t &orig_frame) {
+	//copy frame in case we want to modify it.
+	CAN_message_t frame = orig_frame;
+	
+	//process frame
+	got_frame(frame.id, frame.flags.extended, frame.len,
+	          (BytesUnion *)frame.buf, 0);
+
+	//send to other interface (bridge)
+	Can1.write(frame);
+
 }
 
-void can1_got_frame(CAN_FRAME *frame) {
-	got_frame(frame,1);
+void can1_got_frame_teensy(const CAN_message_t &orig_frame) {
+	//copy frame in case we want to modify it.
+	CAN_message_t frame = orig_frame;
+	
+	//process frame
+	got_frame(frame.id, frame.flags.extended, frame.len,
+	          (BytesUnion *)frame.buf, 1);
+
+	//send to other interface (bridge)
+	Can0.write(frame);
+
 }
+
+#else
+void can0_got_frame_due(CAN_FRAME *frame) {
+	//process frame
+	got_frame(frame->id, frame->extended, frame->length, &(frame->data), 0);
+	
+	//send to other interface (bridge)
+	Can1.sendFrame(*frame);
+}
+
+void can1_got_frame_due(CAN_FRAME *frame) {
+	//process frame
+	got_frame(frame->id, frame->extended, frame->length, &(frame->data), 1);
+
+	//send to other interface (bridge)
+	Can0.sendFrame(*frame);
+}
+#endif
 
 void setup()
 {
+	delay(5000);
 	Serial.begin(115200);
+	Serial.println("j1939 CAN bridge sniffer.");
+
+#ifdef TEENSY
+	//Teensy FlexCAN_T4 setup
+	Can0.begin();
+	Can0.setBaudRate(250000);
+	Can0.enableFIFO();
+	Can0.enableFIFOInterrupt();
+	Can0.onReceive(can0_got_frame_teensy);
+	Can0.enableMBInterrupts(FIFO);
+	Can0.enableMBInterrupts();
+
+	Can1.begin();
+	Can1.setBaudRate(250000);
+	Can1.enableFIFO();
+	Can1.enableFIFOInterrupt();
+	Can1.onReceive(can1_got_frame_teensy);
+	Can1.enableMBInterrupts(FIFO);
+	Can1.enableMBInterrupts();
+#else
 	Can0.begin(CAN_BPS_250K);
 	Can1.begin(CAN_BPS_250K);
 
@@ -114,11 +219,17 @@ void setup()
 		Can1.setRXFilter(0,0,true);
 	}
 
-	Can0.attachCANInterrupt(can0_got_frame);
-	Can1.attachCANInterrupt(can1_got_frame);
+	Can0.attachCANInterrupt(can0_got_frame_due);
+	Can1.attachCANInterrupt(can1_got_frame_due);
+#endif
 }
 
 void loop() 
 {
+#ifdef TEENSY
+	//process collected frames
+	Can0.events();
+	Can1.events();
+#endif
 }
 
