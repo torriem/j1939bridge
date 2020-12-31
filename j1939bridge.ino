6f11866bb00370924aa2637a504fa2790ac21e47
Michael Torrie <torriem@gmail.com>
2020-12-31 12:39:40 -0700

    extra buffer length for making hex codes

--- j1939bridge.ino
+++ j1939bridge.ino
@@ -29,12 +29,12 @@
 #include "canframe.h"
 
 #ifdef TEENSY
-FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
-FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can1;
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_256> Can0;
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_256> Can1;
 #endif
 
 static inline void print_hex(uint8_t *data, int len) {
-	char temp[4];
+	char temp[10];
 	for (int b=0;b < len; b++) {
 		sprintf(temp, "%.2x",data[b]);
 		Serial.print(temp);
@@ -88,10 +88,12 @@ void got_frame(CANFrame &frame, uint8_t which_interface)
 
 	//could filter out what we want to look at here on any of these
 	//variables.
+  
 	if(which_interface)
 		Serial.print("1->0,");
 	else
 		Serial.print("0->1,");
+
 
 	Serial.print(PGN);
 	Serial.print(",");
@@ -101,20 +103,34 @@ void got_frame(CANFrame &frame, uint8_t which_interface)
 	Serial.print(",");
 	Serial.print(destaddr);
 	Serial.print(",");
+  Serial.print(frame.get_length());
+  Serial.print(",");
+
+  //for(int i=0; i < frame.get_length(); i++)
+  //{
+  // Serial.print(frame.get_data()->bytes[i],HEX);
+  //}
+
+  //Serial.println();
+  
 	print_hex(frame.get_data()->bytes, frame.get_length());
+ 
 
 	//example of a decode
 	if (PGN == 65267) { //vehicle position message
 		//latitude
-		Serial.print(frame.get_data()->uint32[0] / 10000000.0 - 210.0);
+    uint32_t latitude = frame.get_data()->uint32[0];
+    uint32_t longitude = frame.get_data()->uint32[1];
+    
+		Serial.print((double)(latitude / 10000000.0 - 210.0), 7);
 		Serial.print(",");
 		//longitude
-		Serial.println(frame.get_data()->uint32[1] / 10000000.0 - 210.0);
+		Serial.println((double)(longitude / 10000000.0 - 210.0),7);
 	}
 	//if (PGN == 1792) {
 	//	frame.get_data()->bytes[1] = 0x99;
 	//}
-
+ 
 	if (which_interface == 1) {
 		//send to other interface (bridge)
 #ifdef TEENSY
@@ -137,17 +153,20 @@ void got_frame(CANFrame &frame, uint8_t which_interface)
 void can0_got_frame_teensy(const CAN_message_t &orig_frame) {
 	//copy frame in case we want to modify it.
 	CANFrame frame = orig_frame;
-	
+  //Serial.println("on 0");	
 	//process frame
 	got_frame(frame, 0);
+  //Can1.write(orig_frame);
 }
 
 void can1_got_frame_teensy(const CAN_message_t &orig_frame) {
 	//copy frame in case we want to modify it.
 	CANFrame frame = orig_frame;
+  //Serial.println("on 1");
 	
 	//process frame
 	got_frame(frame, 1);
+  //Can0.write(orig_frame);
 }
 
 #else
@@ -178,21 +197,24 @@ void setup()
 	//Teensy FlexCAN_T4 setup
 	Can0.begin();
 	Can0.setBaudRate(250000);
-	Can0.setMaxMB(16);
-	//Can0.enableFIFO();
-	//Can0.enableFIFOInterrupt();
+	//Can0.setMaxMB(1);
+	Can0.enableFIFO();
+	Can0.enableFIFOInterrupt();
 	Can0.onReceive(can0_got_frame_teensy);
-	//Can0.enableMBInterrupts(FIFO);
+	Can0.enableMBInterrupts(FIFO);
 	Can0.enableMBInterrupts();
 
 	Can1.begin();
 	Can1.setBaudRate(250000);
-	Can1.setMaxMB(16);
-	//Can1.enableFIFO();
-	//Can1.enableFIFOInterrupt();
+	//Can1.setMaxMB(1);
+	Can1.enableFIFO();
+	Can1.enableFIFOInterrupt();
 	Can1.onReceive(can1_got_frame_teensy);
-	//Can1.enableMBInterrupts(FIFO);
+	Can1.enableMBInterrupts(FIFO);
 	Can1.enableMBInterrupts();
+
+  Can0.mailboxStatus();
+  Can1.mailboxStatus();
 #else
 	Can0.begin(CAN_BPS_250K);
 	Can1.begin(CAN_BPS_250K);
@@ -215,4 +237,3 @@ void loop()
 	Can1.events();
 #endif
 }
-
